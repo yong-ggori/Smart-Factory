f9a53e172a74aeeda57a69711d0e64c3658f6813
jhl9412 <jhunlee9412@gmail.com>
2022-07-22 19:47:37 +0900

    update navigation package, iot_navigation.cpp/ add global_planner_params.yaml

--- turtelbot3/my_navigation_goals/src/iot_navigation_loop.cpp
+++ turtelbot3/my_navigation_goals/src/iot_navigation_loop.cpp
@@ -9,16 +9,40 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define INETADDR 10.10.141.000
+#define INETADDR 10.10.141.34
 #define PORT 5000
 
 void error_handling(const char *message);
 
 
+
+move_base_msgs::MoveBaseGoal goal1;
+move_base_msgs::MoveBaseGoal goal2;
+//Goal 1 - goal_pose
+goal1.target_pose.pose.position.x = 9.5;
+goal1.target_pose.pose.position.y = 1.5;
+goal1.target_pose.pose.position.z = 1.5;
+  	
+goal1.target_pose.pose.orientation.x = 1.0;
+goal1.target_pose.pose.orientation.y = 1.0;
+goal1.target_pose.pose.orientation.z = 1.0;
+goal1.target_pose.pose.orientation.w = 1.0;
+
+//Goal 2 - initial pose
+goal2.target_pose.pose.position.x = 9.5;
+goal2.target_pose.pose.position.y = 1.5;
+goal2.target_pose.pose.position.z = 1.5;
+  	
+goal2.target_pose.pose.orientation.x = 1.0;
+goal2.target_pose.pose.orientation.y = 1.0;
+goal2.target_pose.pose.orientation.z = 1.0;
+goal2.target_pose.pose.orientation.w = 1.0;
+
+
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 int main(int argc, char** argv){
-//flag=0 then start_point to goal_point, flag=1 then goal_point to start_point
+	//flag=0 then start_point to goal_point, flag=1 then goal_point to start_point
 	int flag = 0;
 //socket client
 	int sock=socket(PF_INET, SOCK_STREAM, 0);
@@ -36,19 +60,18 @@ int main(int argc, char** argv){
 
 	char message[30];
 	int str_len;
-
-//register node
+//ROS part
+	//register node
 	ros::init(argc, argv, "simple_navigation_goals");
 	  
-//tell the action client that we want to spin a thread by default
+	//tell the action client that we want to spin a thread by default
 	MoveBaseClient ac("move_base", true);
 	
-//wait for the action server to come up
+	//wait for the action server to come up
 	while(!ac.waitForServer(ros::Duration(5.0))){
 		ROS_INFO("Waiting for the move_base action server to come up");
 	}
 	while(1){
-		move_base_msgs::MoveBaseGoal goal;
 		
 		goal.target_pose.header.frame_id = "base_link";
 		goal.target_pose.header.stamp = ros::Time::now();
@@ -64,36 +87,19 @@ int main(int argc, char** argv){
 
 
 		//flag =0 then s_pt -> g_pt else g_pt -> s_pt
-		if(flag==0){
-			goal.target_pose.pose.position.x = 9.5;
-			goal.target_pose.pose.position.y = 1.5;
-			goal.target_pose.pose.position.z = 1.5;
-			  	
-			goal.target_pose.pose.orientation.x = 1.0;
-			goal.target_pose.pose.orientation.y = 1.0;
-			goal.target_pose.pose.orientation.z = 1.0;
-			goal.target_pose.pose.orientation.w = 1.0;
-		}
-		else{
-			goal.target_pose.pose.position.x = 9.5;
-			goal.target_pose.pose.position.y = 1.5;
-			goal.target_pose.pose.position.z = 1.5;
-			  	
-			goal.target_pose.pose.orientation.x = 1.0;
-			goal.target_pose.pose.orientation.y = 1.0;
-			goal.target_pose.pose.orientation.z = 1.0;
-			goal.target_pose.pose.orientation.w = 1.0;
-
-		}
+		if(flag==0)
+			ac.sendGoal(goal1);
+		else
+			ac.sendGoal(goal2);
+		
 		ROS_INFO("Sending goal");
-		ac.sendGoal(goal);
 		
 		ac.waitForResult();
 		
 		if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-			ROS_INFO("Hooray, the base moved 1 meter forward");
+			ROS_INFO("arrived");
 	  	else
-	    	ROS_INFO("The base failed to move forward 1 meter for some reason");
+	    	ROS_INFO("failed");
 	}
 
 	close(sock);
